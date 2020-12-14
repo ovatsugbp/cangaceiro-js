b697fedfee4c8e0ea696689022ebab1acd94a78b
Gustavo G <gustavogoullart@hotmail.com>
2020-12-14 16:21:58 -0300

    working with reading arrays

--- client/app/controllers/NegociacaoController.js
+++ client/app/controllers/NegociacaoController.js
@@ -7,19 +7,28 @@ class NegociacaoController{
         this._inputData = $("#data");
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
+        this._negociacoes = new Negociacoes();
     }
     
     adiciona(event){
         event.preventDefault();
         
-        let negociacao = new Negociacao(
+        
+        this._negociacoes.adiciona(this._criaNegociacao());
+        this._limpaForm();
+    }
+
+    _limpaForm(){
+        this._inputData.value = '';
+        this._inputQuantidade.value = 1;
+        this._inputValor.value = 0.0;
+        this._inputData.focus();
+    }
+    _criaNegociacao(){
+        return new Negociacao(
             DateConverter.paraData(this._inputData.value),
             parseInt(this._inputQuantidade.value),
             parseFloat(this._inputValor.value)
         );
-        let diaMesAno = DateConverter.paraTexto(negociacao.data);
-
-        console.log(negociacao.data);
-        console.log(diaMesAno);
     }
 }
